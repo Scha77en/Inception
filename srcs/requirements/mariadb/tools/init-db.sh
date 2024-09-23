c6ae0d82b18fd7092419f7a87ca38000074c32f1
Scha77en <aouhbi@e1r3p14.1337.ma>
2024-09-23 21:20:12 +0100

    mariadb server no recognized error ..

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -5,25 +5,34 @@
 # sed -i "s|bind-address\s*=.*|bind-address = 0.0.0.0|" /etc/mysql/mariadb.cnf
 
 set -e
+# srcs/requirements/mariadb/tools/init-db.sh
 
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+# sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
+# sed -i "s|bind-address\s*=.*|bind-address = 0.0.0.0|" /etc/mysql/mariadb.cnf
 
-mysqld --user=mysql --bootstrap << EOF
-USE mysql;
-FLUSH PRIVILEGES;
+# yes
+# mysql_install_db --user=mysql --ldata=/var/lib/mysql
+service mariadb start
+echo a
+mysql_secure_installation << EOF
+n
+${MYSQL_PASSWORD}
+${MYSQL_PASSWORD}
+y
+n
+n
+n
+n
+EOF
 
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DROP DATABASE IF EXISTS test;
-DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-
+mariadb -u root -p${MYSQL_PASSWORD} << EOF
+echo b
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-
 FLUSH PRIVILEGES;
 EOF
 
-exec mysqld --user=mysql
+mysqladmin -u root -p${MYSQL_PASSWORD} shutdown
+
+mariadbd
