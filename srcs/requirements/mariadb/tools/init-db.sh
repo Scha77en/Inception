7438875291e2f6a523b83813c8c773daeb30c10d
Scha77en <aouhbi@e1r12p2.1337.ma>
2024-09-24 00:01:35 +0100

    all containers are running ..

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,32 +1,15 @@
 #!/bin/bash
-# srcs/requirements/mariadb/tools/init-db.sh
-
-# sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
-# sed -i "s|bind-address\s*=.*|bind-address = 0.0.0.0|" /etc/mysql/mariadb.cnf
 
 set -e
-# srcs/requirements/mariadb/tools/init-db.sh
 
-# sed -i "s|# port = 3306|port = 3306|1" /etc/mysql/mariadb.cnf
-# sed -i "s|bind-address\s*=.*|bind-address = 0.0.0.0|" /etc/mysql/mariadb.cnf
-
-# yes
-# mysql_install_db --user=mysql --ldata=/var/lib/mysql
 service mariadb start
-echo a
-mysql_secure_installation << EOF
-n
-${MYSQL_PASSWORD}
-${MYSQL_PASSWORD}
-y
-n
-n
-n
-n
-EOF
 
-mariadb -u root -p${MYSQL_PASSWORD} << EOF
-echo b
+mariadb -u root <<EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -2,24 +2,40 @@
 # srcs/requirements/wordpress/tools/entrypoint.sh
 
 # Wait for MariaDB to be ready
-until mysqladmin ping -h"mariadb" --silent; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 2
-done
-
+sleep 2
+# until mysqladmin ping -h"mariadb" --silent; do
+#     echo "Waiting for MariaDB to be ready..."
+# done
+service php7.4-fpm start
 # Check if WordPress files are already downloaded
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-    echo "WordPress not found. Downloading WordPress..."
-    wp core download --path=/var/www/wordpress/ --allow-root
-else
-    echo "WordPress is already installed."
-fi
+# if [ ! -f /var/www/html/wp-config.php ]; then
+# echo "WordPress not found. Downloading WordPress..."
+# wp core download --path=/var/www/html/ --allow-root
 
-# Check if WordPress is already installed
-if ! wp core is-installed --allow-root; then
-    wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-    wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --allow-root
-fi
+
+
+wp core install --path=/var/www/html/ \
+        --url=${DOMAIN_NAME} \
+        --title=${WP_TITLE} \
+        --admin_user=${WP_ADMIN_USER} \
+        --admin_password=${WP_ADMIN_PASSWORD} \
+        --admin_email=${WP_ADMIN_EMAIL}  \
+        --allow-root
+
+wp user create ${WP_USER} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --role=author --allow-root
+# else
+    # echo "WordPress is already installed."
+# fi
+chown -R www-data:www-data /var/www/html
+# echo "WordPress is ready."
+service php7.4-fpm stop
+# yes
+echo done
+php-fpm7.4 -F
+
+# # Check if WordPress is already installed
+# if ! wp core is-installed --allow-root; then
+# fi
 
 # Start PHP-FPM
-exec "$@"
+# exec "$@"
